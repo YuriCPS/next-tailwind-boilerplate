b39a037ba91a9205b190cc051849e5b362ab4919
Yuri Carvalho <yuricps@gmail.com>
2022-04-24 04:06:19 -0300

    feat: create stories of button component

--- src/components/Button/stories.tsx
+++ src/components/Button/stories.tsx
@@ -14,3 +14,48 @@ Default.args = {
   color: 'amber',
   size: 'medium'
 }
+
+export const Blue: Story<ButtonProps> = (args) => <Button {...args} />
+
+Blue.args = {
+  href: '#',
+  text: 'Button',
+  color: 'blue',
+  size: 'medium'
+}
+
+export const Green : Story<ButtonProps> = (args) => <Button {...args} />
+
+Green.args = {
+  href: '#',
+  text: 'Button',
+  color: 'green',
+  size: 'medium'
+}
+
+export const Neutral : Story<ButtonProps> = (args) => <Button {...args} />
+
+Neutral.args = {
+  href: '#',
+  text: 'Button',
+  color: 'neutral',
+  size: 'medium'
+}
+
+export const Red : Story<ButtonProps> = (args) => <Button {...args} />
+
+Red.args = {
+  href: '#',
+  text: 'Button',
+  color: 'red',
+  size: 'medium'
+}
+
+export const Violet : Story<ButtonProps> = (args) => <Button {...args} />
+
+Violet.args = {
+  href: '#',
+  text: 'Button',
+  color: 'violet',
+  size: 'medium'
+}
